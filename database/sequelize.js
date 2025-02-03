4526d4a707a4f63bed17e493da9959f265b1a3af
Sodade16N <daniellaadebola16@gmail.com>
2025-02-03 13:23:47 +0100

    done

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('sql8759379', 'sql8759379', 'sql8759379', {
+const sequelize = new Sequelize('sql8759379', 'sql8759379', 'x4njfdipLh', {
   host: 'sql8.freesqldatabase.com',
   dialect:'mysql'
 });
